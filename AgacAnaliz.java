2be1f0e6a33a1892b885c9bed0990ec6737ed2f6
natavanmirzayeva <mirzayevanatavan@gmail.com>
2017-03-24 19:59:52 +0200

    Update AgacAnaliz.java

--- AgacAnaliz.java
+++ AgacAnaliz.java
@@ -1,124 +1,155 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-package agacveriyapısıanaliz;
+package agacanaliz;
 
-
-import static java.lang.Math.random;
-import static java.lang.StrictMath.random;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
-import java.util.Set;
-import java.util.TreeSet;
+
 /**
  *
-
-*
+ * @author BM
  */
-
-
- 
-
-
-public class AgacVeriYapısıAnaliz {
+public class AgacAnaliz {
 
     /**
      * @param args the command line arguments
      */
     
-     public static double randomFill(){
-        Random rand = new Random();
-        int randomNum = rand.nextInt();
-        return randomNum;
-    }
-    
-    public static ArrayList<Integer> randomArrayList(int n)
-{
-        ArrayList<Integer> list = new ArrayList<>();
-        Random random = new Random();
-
-        for (int i = 0; i < n; i++)
-        {
-            list.add(random.nextInt(255));
-        }
-        return list;
-}
-    
-   public static int height(TreeNode n)
-{
-    if(n == null)
-        return 0;
-    return 1 + Math.max(height((TreeNode) n.left), height((TreeNode) n.right));
-
-} 
+    static TreeNode root; 
    
    
-    
-    
-    
-    public static void main(String[] args) {
-//        Random random = new Random();
-//        TreeNode node = new TreeNode("12");
-//        TreeNode node1 = new TreeNode();
-//        
-//        
-//        
-//        ArrayList<Integer> arrayRandom = new ArrayList<>();
-//
-//        
-//        
-//        
-//       Set set = new TreeSet(Collections.reverseOrder());
-//       for (int i = 0, n = 100; i < n; i++) {
-//            node1 = node.addChild((random.nextInt(255)));
-//            set.add(node1.data);
-//           
-//        }
-//       
-//      
-//             System.out.println(height(node1));
-//        
-//       
-//       
-//        System.out.println(set);
-//        System.out.println(((TreeSet) set).comparator());
+      static class TreeNode {
+          int item;      
+          TreeNode left;   
+          TreeNode right;   
+          TreeNode(int x) {
+             item = x;
+          }
+      } 
+   
+   
+      static void treeInsert(int x) {
+         if ( root == null ) {
+             root = new TreeNode( x );
+             return;
+          }
+          TreeNode runner; 
+          runner = root;   
+          while (true) {
+             if ( x < runner.item ) {
+                if ( runner.left == null ) {
+                   runner.left = new TreeNode( x );
+                   return;
+                }
+                else
+                   runner = runner.left;
+             }
+             else {
+                if ( runner.right == null ) {
+                   runner.right = new TreeNode( x );
+                   return;  
+                }
+                else
+                   runner = runner.right;
+              }
+          } 
+      }  
+   
+   
+      static int countLeaves(TreeNode node) {
+          if (node == null)
+             return 0;
+          else if (node.left == null && node.right == null)
+             return 1; 
+          else
+             return countLeaves(node.left) + countLeaves(node.right);
+      } 
+      
+      
+      static int sumOfLeafDepths( TreeNode node, int depth ) {
+          if ( node == null ) {
+             return 0;
+          }
+          else if ( node.left == null && node.right == null) {
+             return depth;
+          }
+          else {
+             return sumOfLeafDepths(node.left, depth + 1) 
+                         + sumOfLeafDepths(node.right, depth + 1);
+          }
+      }
+      
+      
+      static int maximumLeafDepth( TreeNode node, int depth ) {
+          if ( node == null ) {
+             return 0;
+          }
+          else if ( node.left == null && node.right == null) {
+               
+             return depth;
+          }
+          else {
+             int leftMax = maximumLeafDepth(node.left, depth + 1);
+             int rightMax =  maximumLeafDepth(node.right, depth + 1);
+             if (leftMax > rightMax)
+                return leftMax;
+             else
+                return rightMax;
+          }
+      } 
+      
+      
+      public static void PrintTree(TreeNode tree) {
         
-        
-        BinaryTree tree = new BinaryTree();
-        BinaryTree tree1 = new BinaryTree();
-        Random random = new Random();
-        // Let us create a binary tree shown in above diagram
-        tree.root = new Node(random.nextInt(255));
-        tree.root.left = new Node(random.nextInt(255));
-        tree.root.right = new Node(random.nextInt(255));
-        tree.root.left.left = new Node(random.nextInt(255));
-        tree.root.left.right = new Node(random.nextInt(255));
-        System.out.println("Derinlik: " + tree.treeHeight(tree.root));
-        int tekrar = 0;
-        
-        tree1.root = new Node(random.nextInt(255));
-        tree1.root.left = new Node(random.nextInt(255));
-        tree1.root.right = new Node(random.nextInt(255));
-        tree1.root.left.left = new Node(random.nextInt(255));
-        tree1.root.left.right = new Node(random.nextInt(255));
-        System.out.println("Derinlik: " + tree.treeHeight(tree.root));
-        
-        
-        
-        ArrayList<Integer> dizi = new ArrayList<>();
-        for (int i = 0; i < dizi.size(); i++) {
-            dizi.set(i,tree.root.data);
-            System.out.println(dizi.get(i));
+            System.out.println(tree);
+
+            if (root.left != null) {
+                PrintTree(tree.left);
+            } 
+
+            if (root.right != null){
+                PrintTree(tree.right);
+            }
+
         }
+      
+      public static void main(String[] args) {
+          ArrayList<Integer> aa = new ArrayList<Integer>();
+          
+         root = null;  
+                 
+         
+        int esitmi = 0;
+         for (int i = 0; i < 20; i++){
+            Random rand = new Random();
+
+            int eleman = rand.nextInt(50) + 1; 
+             for (int j = 0; j < aa.size(); j++) {
+                 if(aa.get(j) == eleman) esitmi++;
+                
+                 
+             }
+            
+             if(esitmi==0) aa.add(eleman);
+
+         }
+          for (int i = 0; i < aa.size(); i++) {
+              treeInsert(aa.get(i));
+            System.out.println("Elemanlar: "+aa.get(i));
+          }
+         
+          System.out.println("ağaç");
+          System.out.println(root.item);
+          
+         int leafCount = countLeaves(root);
+         int depthSum = sumOfLeafDepths(root,0);
+         int depthMax = maximumLeafDepth(root,0);
+         double averageDepth = ((double)depthSum) / leafCount;
+         System.out.println("deptSum"+depthSum);
         
-        
-        
-        
-        
-    }
-    
+         
+         System.out.println("Number of leaves:         " + leafCount);
+         System.out.println("Average depth of leaves:  " + averageDepth);
+         System.out.println("Maximum depth of leaves:  " + depthMax);
+   
+      } 
 }
